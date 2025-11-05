ef333abb4e59166eeee5c1ff631d7ab40c4188fd
djalabz <djalabz@gmail.com>
2025-11-05 14:49:50 +0100

    :sparkles: Route du login complète

--- routes/user.js
+++ routes/user.js
@@ -1,55 +1,101 @@
 
 const app = require('express')
 const router = app.Router();
+const db = require('../config/db');
+const bcrypt = require('bcrypt')
 
 // ENSEMBLE DE ROUTES LIEES AU USER : /user/nomDeLaRoute
-router.post("/message", (req, res) => {
-    console.log(req.body)
-})
-
-router.post("/search", (req, res) => {
-    // On doit recevoir un message du front 
-    // On le récupère et on vérifie si des villes dans violles_france_free ont un nom qui correspond au message 
-    // On renvoit vers le front la liste des villes si il y en a sinon un message d'erreur 
-    // BONUS : lorsque l'on réussit on renvoit un code 200 sinon 500
-    
-    // Pour éxecuter des requetes SQL, on utilise la méthode query sur notre objet DB
-
-    // 1 - On récupère le corps de la req (aka son contenu)
-    const { message } = req.body
-    console.log("req du client : " + message)
-
-    // On explicite notre requete SQL
-    const sql = "SELECT * FROM villes_france_free WHERE ville_nom = ?"
-
-    // On éxecute la requete 
-    db.query(sql, message, (error, results) => {
-        if (error) {
-            console.log(error)
-            return res.status(500).json({ message: 'Erreur lors de l\'éxecution de la requete SQL' })
-
-        } else {
-            console.log(results)
-            return res.status(200).json({ response : results })  
-        }
-    })
-})
-
 router.post("/signup",  (req, res) => {
-    console.log(req.body)
     // Coder le process pour le signup
+    console.log(req.body)
 
     // On récupère les infos transmises par le front : username, email, password
+    const { name, email, password } = req.body
     
     // Username pris ? Email pris ? -> si oui on informe le front et on annule le processus
+    const sql = "SELECT * FROM users WHERE email = ? OR username = ?"
 
-    // On va ensuite hasher le mot de passe avec Bcrypt (à ajouter via NPM)
+    db.query(sql, [email, name], (error, results) => {
+        if (error) {
+            res.status(500).send("Erreur lors de la vérification des infos")
+        } else {
+            console.log(results)
+
+            // Si on a un tableau avec des infos dans results c'est qu'il y a des doublons !
+            if (results.length) {
+                // On veut savoir si il s'agit du username ou du mail 
+                if (email === results[0].email) {
+                    res.send("Le mail est déjà pris !")
+                } else if (name === results[0].username) {
+                    res.send("Le nom est déjà pris !")
+                }
+
+            } else {
+
+                // On vient hasher le mdp 
+                bcrypt.hash(password, 10, (error, hash) => {
+                    if (error) {
+                        res.status(500).send("Erreur lors du hashage du password")
+                    } else {
+                        console.log(hash)
+
+                        const sqlSignup = "INSERT INTO users(username, email, password_hash) VALUES(?, ?, ?)"
+
+                        db.query(sqlSignup, [name, email, hash], (error, results) => {
+                            if (error) {
+                                res.status(500).send("Erreur lors de l'ajout du user en BDD")
+                            } else {
+                                console.log(results)
+                                res.status(200).send("User ajouté avec succès !")
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    })
+
+    // On va ensuite hasher le mot de passe avec Bcrypt (, à ajouter via NPM)
 
     // Via une req SQL (avec les ?) on enregistre les infos en BDD 
     // -> pkoi pas avec un try et catch (pour gérer les erreurs en cas de problemes)
 
     // Quand on recoit la confirmation de la BDD que nos infos sont bien enregistrée
     // on transmet la réussite du process au front avec le code adequat
+
+})
+
+router.post("/login", (req, res) => {
+    // On recup les infos de la req soit le username, mail et password (req.body)
+    const { email, password } = req.body
+
+    // On vient ensuite faire notrre requete préparée qui vient vérifier le mail et le password
+    let sql = "SELECT * FROM users WHERE email = ?;"
+
+    // Si on a un retour positif alors la personne est login (on renvoit un message vers le front)
+    db.query(sql, [email, password], (err, results) => {
+        if (err) {
+            res.status(500).send("Erreur lors de la recherche de user")
+        } else {
+            if (email === results[0].email) {
+                // Vérifier le mdp hashé et celui recu par le user
+                bcrypt.compare(password, results[0].password_hash, (err, results) => {
+                    if (err) {
+                        res.status(500).send("Erreur lors de la vérification des passwords")
+                    } else {
+                        if (results) {
+                            res.send("Le user est Login")
+                        } else {
+                            res.send("Mot de passe invalide")
+                        }
+                    }
+                })
+            } else {
+                res.send("L'email n'existe pas en BDD")
+            }
+        }
+    })
+    
 
 })
 
